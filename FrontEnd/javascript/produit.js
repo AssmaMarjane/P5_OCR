2e94c1dbdb75ef9112efb5825c3a5da44615da6a
Assmam-del <assmam@hotmail.fr>
2020-12-31 23:41:05 +0100

    test ajout js et contenu innerHTML

--- FrontEnd/javascript/produit.js
+++ FrontEnd/javascript/produit.js
@@ -1,1 +1,6 @@
 console.log("ceci est un test");
+const newElt = document.createElement("div");
+let elt = document.getElementById("main");
+
+elt.appendChild(newElt);
+var elementAjoute = element.appendChild("div");
