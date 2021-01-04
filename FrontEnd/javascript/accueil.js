a9ffb0d9e01c025e0af6d1e2d7dbc5e4ef71a17a
Assmam-del <assmam@hotmail.fr>
2021-01-04 17:44:43 +0100

    test requete js page accueil

--- FrontEnd/javascript/accueil.js
+++ FrontEnd/javascript/accueil.js
@@ -3,3 +3,7 @@ console.log("ceci est un test");
 var request = new XMLHttpRequest();
 request.open("GET", "http://localhost:3000/api/teddies ");
 request.send();
+//première requête
+
+//https://github.com/OpenClassrooms-Student-Center/JWDP5.git
+
