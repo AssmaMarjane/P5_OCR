3e83c3277bc1a33a8a991595e30de98813805100
Assmam-del <assmam@hotmail.fr>
2021-01-08 17:43:06 +0100

    ajout methode fetch pr récuperer contenu

--- FrontEnd/javascript/accueil.js
+++ FrontEnd/javascript/accueil.js
@@ -65,6 +65,22 @@ fetch('flowers.jpg')
   const objectURL = URL.createObjectURL(myBlob);
   myImage.src = objectURL;
 });
+var myHeaders = new Headers();
+
+var myInit = { method: 'GET',
+               headers: myHeaders,
+               mode: 'cors',
+               cache: 'default' };
+
+fetch('flowers.jpg',myInit)
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  var objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
+
 */
 
 /*méthode axios
