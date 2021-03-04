1b6808204e48fdd78664258dfba09b16f5014a22
Assmam-del <assmam@hotmail.fr>
2021-03-04 22:59:40 +0100

    test recup value option

--- FrontEnd/javascript/produit.js
+++ FrontEnd/javascript/produit.js
@@ -18,29 +18,32 @@ fetch('http://localhost:3000/api/teddies/' + idProduct)
       //données recup en json passer en HTML
       let myProduct = document.querySelector("#produitSelection");
       myProduct.innerHTML += `<div class="col-lg-12 col-md-6 mb-4">
+
                                <div class="card h-100">
+
                                   <a href="#"><img class="card-img-top" src="${product.imageUrl}" alt=""></a>
                                   <div class="card-body">
                                     <h4 class="card-title"> ${product.name}</h4>
                                     <p class="card-text">${product.description}</p>
                                     <p class="card-text">${product.price}</p>
                                   </div>
+
                                   <form class="form-inline">
                                     <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Couleurs</label>
                                     <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
                                       <option selected></option>
                                     </select>
-
                                     <button id="boutonPanier" type="button" class="btn btn-primary">Ajouter au panier</button>
-
                                   </form>
+
                                 </div>
-                             </div>`;
+
+                              </div>`;
       for (let color of product.colors) {
         let colorList = document.querySelector("#inlineFormCustomSelectPref");
         colorList.innerHTML += ` <option value="${color}">${color}</option>`;
-        
-      }
+  
+      };
       const boutonAjout = document.querySelector('#boutonPanier');    
       boutonAjout.addEventListener('click', function () {         
         boutonAjout.innerHTML = "Produit ajouté au panier!";   
@@ -49,6 +52,13 @@ fetch('http://localhost:3000/api/teddies/' + idProduct)
         //stocker tous les produits en localStorage le produit et sa valeur couleur selectionné + ajout panier
          
       });
+      //let produitChoisi = document.querySelector("#inlineFormCustomSelectPref").option;
+      //console.log(produitChoisi);
+
+      let optionCouleur = document.getElementById("inlineFormCustomSelectPref");
+      let produitChoisi = optionCouleur.options[optionCouleur.selectedIndex].value[0];
+      console.log(optionCouleur);
+      console.log(produitChoisi);
     }
 
   );
