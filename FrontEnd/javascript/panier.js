9f8ab3bf0510fb02f47ee1b36c62ab5d1190149e
Assmam-del <assmam@hotmail.fr>
2021-03-09 13:57:41 +0100

    inserion code js page panier et prépa HTML

--- FrontEnd/javascript/panier.js
+++ FrontEnd/javascript/panier.js
@@ -1,3 +1,36 @@
+// recuperer le tableau localStorage
+// localStorage.getItem('produitChoisi', JSON.stringify(produitChoisi));
+// console.log(localStorage);
 
-localStorage.getItem('produitChoisi', JSON.stringify(produitChoisi));
-console.log(localStorage);
+fetch('http://localhost:3000/api/teddies/')
+  .then(response => response.json())
+  .then(
+    product => {
+      //données recup en json passer en HTML
+      let myProduct = document.querySelector("#listPanier");
+      myProduct.innerHTML += `<div class="col-lg-12 col-md-6 mb-4">
+
+                                <div class="list-group">
+                                    <a href="#" class="list-group-item list-group-item-action active">Ourson</a>
+                                    <a href="#" class="list-group-item list-group-item-action">Ourson1</a>
+                                    <a href="#" class="list-group-item list-group-item-action">Ourson2</a>
+                                    <a href="#" class="list-group-item list-group-item-action">Ourson3</a>
+                                    <a href="#" class="list-group-item list-group-item-action disabled">Total</a>
+                                </div>
+                                <button id="boutonPanier" type="button" class="btn btn-primary"> Valider la commande </button>
+
+                             </div>`;
+     /* for (let  of ) {
+        let  = document.querySelector("#");
+        .innerHTML += ` `; 
+        } ; */
+      const boutonValidation = document.querySelector('#boutonPanier');    
+      boutonValidation.addEventListener('click', function () {         
+        boutonValidation.innerHTML = "Commande validée!";   
+        console.log(boutonValidation);   
+      });
+
+
+    }
+
+  );
