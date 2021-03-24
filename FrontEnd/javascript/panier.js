b0872fe3af612811a5d5cbfe26c0584df0b92cb4
Assmam-del <assmam@hotmail.fr>
2021-03-24 17:39:09 +0100

    récuperation info 'montableau' pur chaque produit

--- FrontEnd/javascript/panier.js
+++ FrontEnd/javascript/panier.js
@@ -1,19 +1,19 @@
 // recuperer le tableau localStorage
-// localStorage.getItem('produitChoisi', JSON.stringify(produitChoisi));
-// console.log(localStorage);
 let tabPanier = localStorage.getItem('monTableau');
-console.log(tabPanier);
-console.log ( JSON.parse(tabPanier));
+//console.log(tabPanier);
+//console.log ( JSON.parse(tabPanier));
+for ( let produits of JSON.parse(tabPanier)){
+  //console.log (produits)
+  let idOfProduct = produits.produit ; 
+  let colorOfProduct = produits.couleur;
+  console.log(idOfProduct + colorOfProduct);
 
-for ( let produit of JSON.parse(tabPanier)){
-  console.log (produit)
-}
+} ; 
 //boucler les produits puis récupérer les infos pour chaque roduits> fetch ciblé par produit cf page produit pour le fetch ciblé
 //faire travail template pr ficher les produit proprement
 
 
-
-fetch('http://localhost:3000/api/teddies/')
+fetch('http://localhost:3000/api/teddies/') //+produits.produit + produits.couleur
   .then(response => response.json())
   .then(
     product => {
@@ -25,8 +25,8 @@ fetch('http://localhost:3000/api/teddies/')
                                 <div class="list-group">
                                     <a href="#" class="list-group-item list-group-item-action active">Ourson</a>
                                     <a href="#" class="list-group-item list-group-item-action"></a>
-                                    <a href="#" class="list-group-item list-group-item-action">Ourson2</a>
-                                    <a href="#" class="list-group-item list-group-item-action">Ourson3</a>
+                                    <a href="#" class="list-group-item list-group-item-action"></a>
+                                    <a href="#" class="list-group-item list-group-item-action"></a>
                                     <a href="#" class="list-group-item list-group-item-action disabled">Total</a>
                                 </div>
                                 <button id="boutonCommande" type="button" class="btn btn-primary"> Valider la commande </button>
