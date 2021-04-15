9946e27988e04e471b247e844ff1daf75e7ee585
Assmam-del <assmam@hotmail.fr>
2021-04-15 15:05:27 +0200

    modification formulaire pge panier

--- FrontEnd/javascript/panier.js
+++ FrontEnd/javascript/panier.js
@@ -87,9 +87,81 @@ for ( let produits of JSON.parse(tabPanier)){
 console.log (products);
 const boutonValidation = document.querySelector('#boutonCommande');    
 boutonValidation.addEventListener('click', function () {         
-boutonValidation.innerHTML = "Commande validée!";
+boutonValidation.innerHTML = `
+          <form class="needs-validation" novalidate>
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="validationTooltip01">First name</label>
+              <input type="text" class="form-control" id="validationTooltip01" placeholder="First name" value="" required>
+              <div class="valid-tooltip">
+                Looks good!
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationTooltip02">Last name</label>
+              <input type="text" class="form-control" id="validationTooltip02" placeholder="Last name" value="" required>
+              <div class="valid-tooltip">
+                Looks good!
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationTooltipUsername">Email</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="validationTooltipUsernamePrepend">@</span>
+                </div>
+                <input type="text" class="form-control" id="validationTooltipEmail" placeholder="Email" aria-describedby="validationTooltipUsernamePrepend" required>
+                <div class="invalid-tooltip">
+                  Please choose a unique and valid email.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="col-md-6 mb-3">
+              <label for="validationTooltip03">Address</label>
+              <input type="text" class="form-control" id="validationTooltip03" placeholder="Address" required>
+              <div class="invalid-tooltip">
+                Please provide a valid Addres.
+              </div>
+            </div>
+            <div class="col-md-3 mb-3">
+              <label for="validationTooltip04">City</label>
+              <input type="text" class="form-control" id="validationTooltip04" placeholder="City" required>
+              <div class="invalid-tooltip">
+                Please provide a valid city.
+              </div>
+            </div>
+            
+          </div>
+          <button class="btn btn-primary" type="submit">Submit form</button>
+          </form>
+
+`;
 console.log(boutonValidation);   
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*alert (
   `<form name="formulaireClient">
