4e56539b2a47a555005032ef0c8cd64604b7b00c
Assmam-del <assmam@hotmail.fr>
2021-05-03 16:22:30 +0200

    ajout foction await pr reset localStorage

--- FrontEnd/javascript/confirmation.js
+++ FrontEnd/javascript/confirmation.js
@@ -1,11 +1,9 @@
 //récupérer l'order_id (num commande) stocker dans le localStorage
 let order = localStorage.getItem('myOrder');
 let orderId = JSON.parse(order);
-console.log(orderId)
 //récupérer le montant total stocker dans le localStorage
 let totalCommande = localStorage.getItem('prixTotal');
 let montantTotal = JSON.parse(totalCommande);
-console.log(montantTotal)
 //afficher montant total et numéro de commande
 let msgOrder = document.querySelector("#messageOder");
 msgOrder.innerHTML += `<p> Merci pour votre achat dans notre boutique.
@@ -13,13 +11,17 @@ msgOrder.innerHTML += `<p> Merci pour votre achat dans notre boutique.
 //récupérer depuis le localStorage le tableau des -id et couleurs des produits pour le reset
 localStorage.getItem('monTableau');
 console.log(localStorage);
-/*if(orderId != "null"){
+async function resetstorage() {
     localStorage.removeItem('monTableau');
     console.log(localStorage);
-  
-}*/
-//localStorage.removeItem('monTableau');
-//console.log(localStorage);
+}
+
+async function resetawait() {
+    const resetStorage = await resetstorage();
+    return resetStorage;
+}
+localStorage.removeItem('monTableau');
+console.log(localStorage);
 /* une page de confirmation de commande, remerciant l'utilisateur pour sa
 commande, et indiquant le prix total et l'identifiant de commande envoyé
 par le serveur.*/
