dba9d7e6783b61656b3ace6c6232bccdc457e913
Assmam-del <assmam@hotmail.fr>
2021-05-08 17:44:03 +0200

    create fonction display pge accueil

--- FrontEnd/javascript/accueil.js
+++ FrontEnd/javascript/accueil.js
@@ -5,24 +5,32 @@ fetch('http://localhost:3000/api/teddies')
     //récupérer objet contenant les produits
     products => {
       //parcourir tous les objets 
-      for (let product of products) {
-        //afficher tous les produits récupérés dans objet et leurs données
-        let homeProduit = document.querySelector("#containerProduct");
-        homeProduit.innerHTML += ` <div  class="col-lg-4 col-md-6 mb-4">
-                            <div class="lienProduit" class="card h-100">
-                              <a href="./Contenu/produit.html?id=${product._id}" ><img class="card-img-top" src=" ${product.imageUrl}" alt=""></a>
-                              <div class="card-body">
-                                <h4 class="card-title">
-                                  <a > ${product.name}</a>
-                                </h4>
-                                <p class="card-text">${product.price}</p>
-                                <p class="card-text">${product.description}</p>
-                              </div>
-                            </div>
-                          </div>`;
-      }
+      displayProducts(products)
     }
   );
+
+function displayProducts (products){
+  for (let product of products) {
+    //afficher tous les produits récupérés dans objet et leurs données
+    let homeProduit = document.querySelector("#containerProduct");
+    homeProduit.innerHTML += ` <div  class="col-lg-4 col-md-6 mb-4">
+                        <div class="lienProduit" class="card h-100">
+                          <a href="./Contenu/produit.html?id=${product._id}" ><img class="card-img-top" src=" ${product.imageUrl}" alt=""></a>
+                          <div class="card-body">
+                            <h4 class="card-title">
+                              <a > ${product.name}</a>
+                            </h4>
+                            <p class="card-text">${product.price}&euro</p>
+                            <p class="card-text">${product.description}</p>
+                          </div>
+                        </div>
+                      </div>`;
+  }
+
+
+}
+
+
 /* Données produits Teddy:
     product._id =  id du produit
     product.imageUrl = la photo du produit 
@@ -31,3 +39,4 @@ fetch('http://localhost:3000/api/teddies')
     product.description = la description du produit
 
 */
+
