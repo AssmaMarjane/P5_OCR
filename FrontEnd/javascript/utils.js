0946acb78d3c280370efb5335b4da9f3f7882b22
Assmam-del <assmam@hotmail.fr>
2021-05-26 11:49:04 +0200

    ajout param numberformat function dividedPrice

--- FrontEnd/javascript/utils.js
+++ FrontEnd/javascript/utils.js
@@ -1,4 +1,6 @@
 function dividedPrice (price){
-    let division = price/100;
-    return division ;
+   // let division = price/100;
+    let prix = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price/100); // je peux mettre l variable division aussi
+    return prix;
     };
+
