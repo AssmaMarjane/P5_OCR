8561ff9f6b2175e15edb2a70ef54b9169318bbd4
Assmam-del <assmam@hotmail.fr>
2021-07-16 15:34:21 +0200

    correction html sigle euro

--- FrontEnd/javascript/confirmation.js
+++ FrontEnd/javascript/confirmation.js
@@ -7,7 +7,7 @@ let montantTotal = JSON.parse(totalCommande);
 //afficher montant total et numéro de commande
 let msgOrder = document.querySelector("#messageOrder");
 msgOrder.innerHTML += `<p> Merci pour votre achat dans notre boutique.</p>
-                       <p>Votre commande d'un montant total de ${dividedPrice(montantTotal)}€ est enregistrée sous le numero suivant ${orderId}.</p>
+                       <p>Votre commande d'un montant total de ${dividedPrice(montantTotal)} est enregistrée sous le numero suivant ${orderId}.</p>
                        <p>Conservez bien ce numèro.</p>
                        <p>Le récapitulatif de votre commande vous a été envoyé par mail.
                        <p>A bientôt sur notre site!</p>`
